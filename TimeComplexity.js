cbf046c9f4c4a658b6a5448c255e184a33605e82
Andy <jihyuk.sung@gmail.com>
2022-01-28 22:34:36 -0700

    Initial Commit

---
+++ TimeComplexity.js
@@ -0,0 +1,43 @@
+function logUpTo(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log(i);
+    }
+}
+//Time Complexity: O(n)
+
+function logAtLeast10(n) {
+    for (let i = 1; i <= Math.max(n, 10); i++) {
+        console.log(i);
+    }
+}
+//Time Complexity: O(n)
+
+function logAtMost10(n) {
+    for (let i = 1; i <= Math.min(n, 10); i++) {
+        console.log(i);
+    }
+}
+//Time Complexity: O(1)
+
+function onlyElementsAtEvenIndex(array) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
+//Time Complexity: O(n)
+
+function subtotals(array) {
+    let subtotalArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let subtotal = 0;
+        for (let j = 0; j <= i; j++) {
+            subtotal += array[j];
+        }
+        subtotalArray.push(subtotal);
+    }
+    return subtotalArray;
+}
